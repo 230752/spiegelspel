7ed3a97443f43a5f3bfda354da324d8f8a258b20
230752 <150030422+230752@users.noreply.github.com>
2024-12-05 13:44:37 +0100

    Add files via upload

---
+++ core/header.php
@@ -0,0 +1,17 @@
+<div id="header-content">
+    <a href="#">
+        <img id="header-logo" src="assets/img/Spiegelspel-Logo.png" alt="weblogo">
+    </a>
+    <div id="header-buttons">
+        <a href="#section2" class="header-button">DOEL SPIEGELSPEL</a>
+        <a href="#section3" class="header-button">HET SPIEGELSPEL</a>
+        <a href="#section4" id="contact-button">CONTACT</a>
+    </div>
+    <img id="menu-icon" src="assets/img/icons/menu.png" alt="menu-btn">
+</div>
+<div id="mobile-menu">
+    <button id="close-menu"><img id="close-icon" src="assets/img/icons/close-icon.png" alt="close-icon"></button>
+    <a href="#section2" class="header-button">DOEL SPIEGELSPEL</a>
+    <a href="#section3" class="header-button">HET SPIEGELSPEL</a>
+    <a href="#section4" id="menu-contact">CONTACT</a>
+</div>
